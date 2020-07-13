be7b72ab7b50e7b5c35286020cc5b1fe04f399a9
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-07-13 15:34:49 -0300

    Updates

---
+++ precache-manifest.7ec08ab38230bc8cc82e5eae90df2513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1707d88e807498201184f80670e5423f",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "f650d269487b82f469a9",
+    "url": "/exercise-tracker/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "f650d269487b82f469a9",
+    "url": "/exercise-tracker/static/js/2.c432a584.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-tracker/static/js/2.c432a584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d350e9977d5bea51ed30",
+    "url": "/exercise-tracker/static/js/main.bed78cb1.chunk.js"
+  },
+  {
+    "revision": "19efbaa7935c6d4a6ff8",
+    "url": "/exercise-tracker/static/js/runtime-main.c3c5cc0f.js"
+  }
+]);
